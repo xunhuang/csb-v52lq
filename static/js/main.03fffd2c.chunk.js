a3978aff2936b7da684464d78ce8a89d74cce1d5
Code Sandbox <hello@codesandbox.io>
2021-04-18 22:43:53 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.03fffd2c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-typescript"]=this["webpackJsonpreact-typescript"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(3),r=(c(8),c(0));function s(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)("h1",{children:"Hello CodeSandbox 122233456 - hello world magic"}),Object(r.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var i=document.getElementById("root");Object(n.render)(Object(r.jsx)(s,{}),i)},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.03fffd2c.chunk.js.map
--- static/js/main.7b443f06.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-typescript"]=this["webpackJsonpreact-typescript"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(3),s=(c(8),c(0));function r(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"Hello CodeSandbox 123"}),Object(s.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var i=document.getElementById("root");Object(n.render)(Object(s.jsx)(r,{}),i)},8:function(e,t,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.7b443f06.chunk.js.map
